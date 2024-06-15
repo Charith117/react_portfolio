982dedce148f250a51ff60f03e27121961676395
Charith Jayanitha <123439615+Charith117@users.noreply.github.com>
2024-06-15 22:32:46 +0530

    new update ccode

---
+++ .history/react-portfolio/src/components/Hero/Hero_20240615212720.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from "./Hero.module.css";
+import {getImageUrl} from"../../util";
+
+const Hero = () => {
+  return (
+    <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi im Charith</h1>
+        <p className={styles.descruption}>
+            Im Full satck developer but im still lerning things so if you need im here
+        </p>
+        <div className={styles.mainBtns}>
+        <a href="jayanithacharith7@gmail.com" className={styles.contactBtn}>Contact me</a>
+        <a href="jayanithacharith7@gmail.com" className={styles.DonloadBtn}>Download CV</a>
+        </div>
+       
+    </div>
+    <img className={styles.heroImg} src={getImageUrl("hero/heroImage.png")} alt="Her" />
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
+  </section>
+  );
+
+};
+
+export default Hero
