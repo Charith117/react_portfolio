13e73f399046fa81d848b06f3d199258d2387110
Charith Jayanitha <123439615+Charith117@users.noreply.github.com>
2024-06-17 23:38:41 +0530

    gf

--- .history/react-portfolio/vite.config_20240617230249.js
+++ .history/react-portfolio/vite.config_20240617230249.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio/', // Replace with your repository name
   plugins: [react()],
-  css: {
-    modules: {
-      localConvention: "camelCase",
+  css:{
+    modules:{
+      localConvention:"camelCase",
+
     },
   },
 })
--- .history/react-portfolio/vite.config_20240617233748.js
+++ .history/react-portfolio/vite.config_20240617233748.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio/', // Replace with your repository name
+  base: '/git subtree push --prefix dist origin gh-pages/', // Replace with your repository name
   plugins: [react()],
   css: {
     modules: {
